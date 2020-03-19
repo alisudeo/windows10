fbc41b1d9bffff12159f4913ae3fda8395d3250e
Alisu <a.docalves@gmail.com>
2020-03-19 20:31:03 -0300

    *Atualizado ícone de informação; *Atulizada a imagem do usuário; +Adicionado animação de boot; +Adicionada animação ao aparecer a hora na tela de bloqueio pela primeira vez; +Adicionado teclado virtual à tela de senha; +Adicionada informação sobre drivers; +Adicionada função de bloqueio de tela, no ícone de força localizado no menu do windows.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
+function openBoot(){
+    document.getElementById('boot').style.opacity = '0';
+    setTimeout(() => {
+        document.getElementById('boot').style.display = 'none';
+    }, 400);
+}
+
 function monthString(x){
     switch(x){
         case 0:
@@ -139,7 +146,7 @@ function openPassword(x){
 function checkPassword(){
     x = document.getElementById('password-field').value;
     
-    if(x == 'Windows10'){
+    if(x.toUpperCase() == 'WINDOWS10'){
         document.getElementById('lock-password').style.top = '-100vh';
         return false;
     }
@@ -153,8 +160,12 @@ function openHint(x){
     switch(x){
         case 1:
             var y = "<p>Dados do usuário&nbsp;<img src='img/regedit.png'></p><p>Os dados do usuário, como nome, e-mail e senha são armazenados em um lugar chamado de 'Registro do Windows', seu banco de dados. Lá estão presentes, também, dados sobre os programas instalados no computador, sendo eles nativos desse sistema operacional ou não.</p><p>Os dados lá guardados são utilizados de diferentes formas, como para verificação e atualização da versão dos programas instalados ou para o login do usuário no computador. A propósito, use 'Windows10' como senha.</p>";
+            break;
         case 2:
             var y = "<p>Internet&nbsp;<img src='img/internet-globe.png'></p><p>A internet nada mais é do que um monte de cabos ligando máquinas à servidores que, por sua vez, se ligam a outras máquinas, compartilhando informações entre si.</p><p>O google, serviço de pesquisa mundialmente conhecido, se parece mais com uma biblioteca, na verdade. Ele indexa os arquivos, como fotos e páginas, presentes nos servidores e exibe os que se relacionam com a pesquisa feita pelo usuário.</p><p>As páginas lidas pelo navegador se parecem com blocos de textos cheios de comandos, que dizem ao navegador onde, como e o que exibir. Existem vários navegadores e cada um deles tem sua própria maneira de customizar o que é exibido para melhor entendimento e agrado do usuário, sendo o mais famoso o Google Chrome.</p><p>Assim como os humanos, máquinas também possuem suas linguas e cada uma delas possui o mesmo proósito: dizer ao computador o que fazer. As linguagens, como são chamadas,usadas para se comunicar com os navegadores são, principalmente, HTML, CSS e JavaScript. Cada uma delas tem sua própria função, sendo elas: O HTML diz o que vai ser exibido; O CSS diz como e onde vai ser exibido; E o JavaScript muda o que está presente no CSS conforme a navegação do usuário pela página, dando-a vida.</p>";
+            break;
+        case 3:
+            var y = "<p>Drivers&nbsp;<img src='img/mouse.png'></p><p>Uma das muitas funções de um sistema operacional é organizar os drivers do usuário/computador, garantindo a comunicação entre o usuário, por meio do próprio SO, e os programas nele presentes.</p><p>São os drivers, por exemplo, os responsáveis por requisitarem que a impressora imprima o que foi pedido pelo usuário, ou por fazer com que as teclas digitas no teclado sejam interpretadas corretamente pelo Sistema Operacional.</p><p>Imprevistos acontecem e periféricos quebram. Quando um equipamento externo, utilizado pelo usuário, deixa de funcionar corretamente, são dadas opções alternativas ao utilizador, pelo Sistema Operacional, para que a interação ainda seja possível. O teclado virtual é uma função de acessibilidade presente no Windows, desde a versão XP e foi introduzido com o propósito de substituir o teclado físico, caso deixe de funcionar.</p>";
     }
 
     document.getElementById('hint-modal-content').innerHTML = y + "<input type='button' value='Entendi' onclick='closeHint()'><div style='height: 4vh;'></div>";
@@ -169,8 +180,8 @@ function closeHint(){
     document.getElementById('hint-modal').style.opacity = '0';
     setTimeout(() => {
         document.getElementById('hint-modal-holder').style.display = 'none';
+        document.getElementById('hint-modal-content').innerHTML = '';
     }, 400);
-    document.getElementById('hint-modal-content').innerHTML = '';
 }
 
 function openProgram(x){
@@ -179,13 +190,13 @@ function openProgram(x){
             var w = 'img/chrome.png';
             var y = "<img src='img/google.png' id='google-logo'><input type='text' id='google-search' value='Guaxinins lindos' onclick='this.blur()' onfocus='this.blur()'><div class='result-holder'><img src='https://i.pinimg.com/originals/86/f6/8e/86f68eea9858f4e6eed3a7fa8c7a4854.jpg' class='google-result'></div><div class='result-holder'><img src='https://previews.123rf.com/images/a41cats/a41cats1211/a41cats121100055/16385155-cute-raccoon-on-green-grass-nice-close-up-a.jpg' class='google-result'></div><div class='result-holder'><img src='https://i.ytimg.com/vi/IRdivT8pcl4/maxresdefault.jpg' class='google-result'></div>";
             var z = "openHint(2)";
+        break;
     }
 
     document.getElementById('open-program').style.opacity = '1';
     document.getElementById('open-program-img').setAttribute('src', w);
     document.getElementById('program-content').innerHTML = y;
     document.getElementById('program-hint').setAttribute('onclick', z);
-
     document.getElementById('program-holder').style.display = 'block';
     setTimeout(() => {
         document.getElementById('program').style.opacity = '1';
@@ -201,3 +212,61 @@ function closeProgram(){
     }, 400);
     document.getElementById('program-content').innerHTML = '';
 }
+
+function openKeyboard(){
+    document.getElementById('keyboard-modal-holder').style.display = 'block';
+    setTimeout(() => {
+        document.getElementById('keyboard-modal').style.opacity = '1';
+    }, 10);
+}
+
+function closeKeyboard(){
+    document.getElementById('keyboard-modal').style.opacity = '0';
+    setTimeout(() => {
+        document.getElementById('keyboard-modal-holder').style.display = 'none';
+    }, 400);
+}
+
+function addKey(x){
+    aKey = x.innerHTML;
+    var bKey = '';
+    for(var i = 1; i <= aKey.length; i++){
+        if(aKey.charAt(i) != ' '){
+            bKey += aKey.charAt(i);
+        }
+    }
+    document.getElementById('keyboard-password').value += bKey;
+    document.getElementById('password-field').value += bKey;
+}
+
+function removeKey(){
+    x = document.getElementById('keyboard-password').value;
+    y = '';
+    
+    if(x.length != 0){
+        for(var i = 0; i < x.length - 1; i++){
+            y += x.charAt(i);
+        }
+
+        document.getElementById('keyboard-password').value = y;
+        document.getElementById('password-field').value = y;
+    }
+}
+
+function lockScreen(){
+    closeMenu();
+    document.getElementById('password-status').innerHTML = '';
+    document.getElementById('password-field').value = '';
+    document.getElementById('lock-password').style.top = '0vh';
+}
+
+window.onload = function(){
+    horaCerta(); 
+    setTimeout(() => {
+        openBoot(); 
+        setTimeout(() => {
+            document.getElementById('lock-hour').style.width = '38vh';
+            document.getElementById('lock-date').style.width = '38vh';
+        }, 500);
+    }, 500);
+};
